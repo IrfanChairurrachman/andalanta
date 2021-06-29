0fdb763c0fc452e35ebe7ad95cb76a180a9c62f0
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-06-29 15:46:04 +0700

    feat: add settings

---
+++ app/Database/Migrations/2021-06-29-083455_Settings.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Settings extends Migration
+{
+	public function up()
+	{
+		//
+		$this->forge->addField([
+            'setting_id'           => [
+                'type'              => 'BIGINT',
+                'constraint'        => 20,
+                'unsigned'          => TRUE,
+                'auto_increment'    => TRUE
+            ],
+            'setting_name'          => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '100',
+            ],
+			'settinga_status'       => [
+                'type'              => 'ENUM',
+                'constraint'        => "'Open','Close'",
+                'default'           => 'Open'
+            ],
+			'setting_image'         => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '100',
+            ],
+			'setting_contact'       => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '100',
+            ],
+        ]);
+		$this->forge->addKey('setting_id', TRUE);
+        $this->forge->createTable('settings');
+	}
+
+	public function down()
+	{
+		//
+		$this->forge->dropTable('settings');
+	}
+}
